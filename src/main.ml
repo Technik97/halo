ab022bfb4b8c5f5220c435d3afb89fc21e224acd
Aniket <aniket.97.mane@gmail.com>
2025-08-04 01:17:13 +0530

    Testing tape

--- src/main.ml
+++ src/main.ml
@@ -1,16 +1,19 @@
+open Tape
+
 let is_valid_char c = 
   match c with 
   | '>' | '<' | '+' | '-' | '.' | ',' | '[' | ']' -> true
   | _ -> false
 
-let parse (code : string): char list = 
+let _parse (code : string): char list = 
   code 
   |> String.to_seq 
   |> Seq.filter is_valid_char
   |> List.of_seq
 
 let () = 
-  let code = "++Hello World[->+<]" in
-  let instructions = parse code in 
-  List.iter (Printf.printf "%c") instructions;
-  print_newline ()
+  let tape = Tape.init () in 
+  let tape = tape |> increment |> increment |> move_right |> increment in
+  Printf.printf "Left: %s\n" (String.concat ";" (List.map string_of_int tape.left));
+  Printf.printf "Current: %d\n" tape.current;
+  Printf.printf "Right: %s\n" (String.concat ";" (List.map string_of_int tape.right)) 
