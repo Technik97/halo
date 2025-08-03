a93def6f823cae209ed4217302b6acfe4b259493
Aniket <aniket.97.mane@gmail.com>
2025-08-04 00:42:02 +0530

    added is_valid_char and parse functions

--- src/main.ml
+++ src/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+let is_valid_char c = 
+  match c with 
+  | '>' | '<' | '+' | '-' | '.' | ',' | '[' | ']' -> true
+  | _ -> false
+
+let parse (code : string): char list = 
+  code 
+  |> String.to_seq 
+  |> Seq.filter is_valid_char
+  |> List.of_seq
+
